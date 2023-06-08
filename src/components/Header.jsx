0477f6489b09f5914d448a43b3242c6ebbb2c6f4
Pierre-Emmanuel Law-Hune <122879897+PierreL-H@users.noreply.github.com>
2023-06-08 05:29:54 +0200

    implement header component and style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,38 @@
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import classes from "./Header.module.scss";
+import Logo from "../assets/images/logo-red.svg";
 export default function Header() {
   return (
-    <header>
+    <header className={classes.header}>
+      <img src={Logo} alt="" className={classes.header_logo} />
       <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/about"}>About</Link>
+        <ul className={classes.header_list}>
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${classes.header_link} ${
+                  isActive ? classes.header_link___active : null
+                }`
+              }
+              end
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) =>
+                `${classes.header_link} ${
+                  isActive ? classes.header_link___active : null
+                }`
+              }
+            >
+              A Propos
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   );
