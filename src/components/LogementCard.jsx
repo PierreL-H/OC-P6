3e9a1d48005829557ccca69ac349e1ec88150738
Pierre-Emmanuel Law-Hune <122879897+PierreL-H@users.noreply.github.com>
2023-06-09 00:12:37 +0200

    finish Logements and Card styles

--- src/components/LogementCard.jsx
+++ src/components/LogementCard.jsx
@@ -2,9 +2,9 @@ import classes from "./LogementCard.module.scss";
 
 const LogementCard = ({ logement }) => {
   return (
-    <figure>
-      <img src={logement.cover} alt="" />
-      <figcaption>{logement.title}</figcaption>
+    <figure className={classes.Card}>
+      <img src={logement.cover} alt="" className={classes.Card_image} />
+      <figcaption className={classes.Card_caption}>{logement.title}</figcaption>
     </figure>
   );
 };
