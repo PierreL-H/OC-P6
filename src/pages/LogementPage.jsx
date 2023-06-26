b4a78b8442500947bcce2b602b54e6e9551608ed
Pierre-Emmanuel Law-Hune <122879897+PierreL-H@users.noreply.github.com>
2023-06-26 16:17:32 +0200

    fix redirection on invalid id

--- src/pages/LogementPage.jsx
+++ src/pages/LogementPage.jsx
@@ -1,5 +1,5 @@
 import logements from "../data/logements.json";
-import { useNavigate, useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
 import Slideshow from "../components/Slideshow";
 import classes from "./LogementPage.module.scss";
@@ -8,13 +8,17 @@ import LogementDetails from "../components/LogementDetails";
 const LogementPage = () => {
   const params = useParams();
   const logement = logements.find((item) => item.id === params.id);
-  const navigate = useNavigate();
-  useEffect(() => {
-    !logement && navigate("not-found");
-  });
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   !logement && navigate("not-found");
+  // });
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  if (!logement){
+    return <Navigate to='../../not-found' />
+  }
   console.log(logement);
 
   return (
